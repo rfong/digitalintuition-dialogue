a14f74da364fdc4fe00ec8c481b98afefa6a637b
Rachel Fong <rachefo@gmail.com>
2012-05-07 16:43:05 -0400

    more modular, tweaked weights, added concepts

--- divisi_test.py
+++ divisi_test.py
@@ -20,38 +20,38 @@ def main():
   for input_file in input_files:
     fin = open( input_file )
     for line in fin.readlines():
+      line = line.strip()
       hash = line.split('#')[-1]
       line_terms = nltk.word_tokenize( line.split('#')[0] )
       if hash[0]=='D':
         allTerms['doctors'] += line_terms
       elif hash[0]=='P':
         allTerms['patients'] += line_terms
+
+  concepts = [line.strip() for line in open('concepts.txt', 'r').readlines()]
   
-  # weight terms by sqrt of frequency
+  # weight terms by function of frequency
   for group, terms in allTerms.iteritems():
     print group
     freqDict = {}
     for term in terms:
       freqDict[term] = freqDict.get(term, 0) + 1
-    freqDict = {term : math.sqrt(freqDict.get(term)) for term in freqDict.keys()}
+    # let's normalize to keep the final score down
+    freqDict = {term : weight_fn(freqDict.get(term)) / weight_fn(max(freqDict.values())) \
+                for term in freqDict.keys()}
   
     # divisi
     vec = divisi2.DenseVector(np.zeros((150,)))
     for term in terms:
       if term in assocmat.row_labels:
-        vec += assocmat.row_named(term) * 1.0
-    
-    vec = divisi2.DenseVector(np.zeros((150,)))
-    happy = assocmat.row_named('happy')
-    sad = assocmat.row_named('sad')
-    
-    for term in terms:
-      if term in assocmat.row_labels:
-        vec += assocmat.row_named(term) * 1.0
-    similarHappy = happy.dot(vec)
-    similarSad = sad.dot(vec)
-    print "Happy Similarity: " + str(similarHappy)
-    print "Sad Similarity: " + str(similarSad)
+        vec += assocmat.row_named(term) * freqDict.get(term)
+
+    for concept in concepts:
+      concept_vec = assocmat.row_named(concept)
+      print '\t' + concept + ':', str( concept_vec.dot(vec) )
+
+def weight_fn(freq):
+  return math.pow(float(freq), 1.0/2)
 
 def exit():
   print 1/0
